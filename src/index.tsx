d35f443445adbc93856d8e9d1e0ec3929c77b965
neominal.solutions <neominal.solutions@gmail.com>
2024-11-28 09:25:01 +0300

    Hook Forms

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app.component';
 
-import HomePage from './pages/home.page';
 import {
 	createBrowserRouter,
 	Link,
 	Outlet,
 	RouterProvider,
 } from 'react-router-dom';
-import AboutPage from './pages/about.page';
 import Layout from './components/layout.component';
-import UsersPage from './pages/users.page';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -20,32 +16,9 @@ const root = ReactDOM.createRoot(
 const router = createBrowserRouter([
 	{
 		path: '/',
-		Component: Layout, // Aşağıdaki pathler bu layout üzerinden ortak tasarımları alırlar. header,footer,menu,logo, profile, sepet kısayolu.
-		children: [
-			{
-				path: '/',
-				Component: HomePage,
-			},
-			{
-				path: '/about',
-				Component: AboutPage,
-			},
-		],
+		Component: Layout,
+		children: [],
 	},
-	{
-		path: '/admin',
-		element: (
-			<div>
-				<h1>Admin Layout</h1>
-				<Outlet />
-			</div>
-		),
-		children: [{ path: 'users', Component: UsersPage }],
-	},
-	{
-		path: '*',
-		element: <>Sayfa Bulunamadı</>,
-	}, // Her zaman en son değer bu olucak
 ]);
 
 // bu kod blogu içerisinde RouterProvider dan başka kod bloğu olmasın.
